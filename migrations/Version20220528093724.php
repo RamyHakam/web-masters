fafe6eb32effc5a004789fe5cf94a7782ebfcb0f
Ramy Hakam <pencilsoft1@gmail.com>
2022-05-29 01:01:00 +0200

    134

--- migrations/Version20220528093724.php
+++ migrations/Version20220528093724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220528091859 extends AbstractMigration
+final class Version20220528093724 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,12 @@ final class Version20220528091859 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE page ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('UPDATE page SET created_at = NOW(), updated_at = NOW()');
+        $this->addSql('ALTER TABLE page CHANGE created_at created_at DATETIME NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE page DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE page CHANGE created_at created_at DATETIME DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
 }
