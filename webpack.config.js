26c6438481e4c6e5f632b680af21d17149aecbf4
Ramy Hakam <pencilsoft1@gmail.com>
2022-06-26 16:03:53 +0200

    192

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('post', './assets/post.js')
+    .addEntry('form', './assets/form.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
