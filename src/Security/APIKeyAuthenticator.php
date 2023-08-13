053bf0305927a488dfb1905ff864dc24be6e41b5
Ramy Hakam <pencilsoft1@gmail.com>
2023-08-13 21:55:12 +0200

    238

--- src/Security/APIKeyAuthenticator.php
+++ src/Security/APIKeyAuthenticator.php
@@ -2,17 +2,31 @@
 
 namespace App\Security;
 
+use App\Repository\Main\AccountRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class APIKeyAuthenticator extends AbstractAuthenticator implements  AuthenticationEntryPointInterface
+class APIKeyAuthenticator extends AbstractAuthenticator
 {
+    use TargetPathTrait;
+
+    private AccountRepository $accountRepository;
+
+    public function __construct(AccountRepository $accountRepository)
+    {
+        $this->accountRepository = $accountRepository;
+    }
+
     public function supports(Request $request): ?bool
     {
         return  $request->headers->has('X-AUTH-TOKEN');
@@ -20,21 +34,24 @@ class APIKeyAuthenticator extends AbstractAuthenticator implements  Authenticati
 
     public function authenticate(Request $request): Passport
     {
-        // TODO: Implement authenticate() method.
-    }
+         $apiKey = $request->headers->get('X-AUTH-TOKEN');
+          return new SelfValidatingPassport(new UserBadge($apiKey, function ($apiKey) {
+                 $email = base64_decode($apiKey);
+                return $this->accountRepository->findOneBy(['email' => $email]);
+            }));
+          }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName))
+        {
+            return new RedirectResponse($targetPath);
+        }
+        return new JsonResponse(['success' => true]);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        return  new JsonResponse(['message'=>'Authentication Required'],Response::HTTP_UNAUTHORIZED);
+        return new JsonResponse(['success' => false, 'message' => strtr($exception->getMessageKey(), $exception->getMessageData())], 401);
     }
 }
