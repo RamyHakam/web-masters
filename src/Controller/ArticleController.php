8e9214a62ce5e38bf12a3bea7971082b8566d1e4
Ramy Hakam <pencilsoft1@gmail.com>
2023-08-24 23:09:16 +0200

    243

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Main\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/articles",name="app_articles")
+     ** @IsGranted("ROLE_USER")
+     * @return Response
+     */
+    public function articles( ): Response
+    {
+        return $this->render('article.html.twig',['account' => $this->getUser(),'articles' => $this->getUser()->getArticles()]);
+    }
+
+    /**
+     * @Route("/article/{id}/edit", name="article_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        if ($request->isMethod('POST')) {
+            $article->setPhoto($request->request->get('photo'));
+            $article->setContent($request->request->get('content'));
+            $article->setLikes($request->request->getInt('likes'));
+
+            $this->entityManager->persist($article);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Article updated successfully!');
+
+            return $this->redirectToRoute('app_articles');
+        }
+
+        return $this->render('article_edit.html.twig', ['article' => $article]);
+    }
+    /**
+     * @Route("/article/{id}/delete", name="article_delete", methods={"GET", "POST"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($request->isMethod('POST')) {
+
+            $this->entityManager->remove($article);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Article deleted successfully!');
+
+            return $this->redirectToRoute('app_articles');
+        }
+        return $this->render('article_delete.html.twig', ['article' => $article]);
+    }
+}
